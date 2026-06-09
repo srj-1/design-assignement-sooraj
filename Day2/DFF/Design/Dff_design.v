9963b57948d3397ab39c806995a1e926a4ebaa9e
srj-1 <soorajh001@gmail.com>
2026-06-09 21:40:27 +0530

    Update Dff_design.v

--- Day2/DFF/Design/Dff_design.v
+++ Day2/DFF/Design/Dff_design.v
@@ -1,1 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 16:45:49
+// Design Name: 
+// Module Name: DFF
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DFF(input d,rst,clk,output q,qbar);
+sr_flipflop ff (d,~d,rst,clk,q,qbar);
+
+endmodule
 
